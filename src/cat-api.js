6c414023327a21f872aa1a35f0b34fabe870807e
Makarenko Pavlo <cmertnik1986@gmail.com>
2023-06-22 21:34:18 +0300

    fyneli

--- src/cat-api.js
+++ src/cat-api.js
@@ -8,29 +8,29 @@ const API_HEADERS = new Headers({
 
 
 
-export function fetchBreeds(){
-       return fetch(`${API_URL}breeds`,{method:"GET", headers:API_HEADERS})
+export function fetchBreeds() {
+    return fetch(`${API_URL}breeds`, { method: "GET", headers: API_HEADERS })
         .then(response => {
             if (!response.ok) {
                 throw new Error(response.status);
             }
-            
-           return response.json()})
+            return response.json();
+        })
         .then(res => {
-           return res.map(obj => ({
+            return res.map(obj => ({
                 id: obj.id,
                 name: obj.name
-            }))
+            }));
         })
-        .catch(e =>console.log(e)); 
-    }
+        .catch(e => console.log(e));
+}
 
-    export function fetchCatByBreed(breedId){
-        // console.log(breedId);
-        return fetch(`${API_URL}images/search?breed_ids=${breedId}`, {method:"GET",headers:API_HEADERS}).then(req => 
-            {if(!req.ok){
+export function fetchCatByBreed(breedId) {
+    return fetch(`${API_URL}images/search?breed_ids=${breedId}`, { method: "GET", headers: API_HEADERS })
+        .then(req => {
+            if (!req.ok) {
                 throw new Error(req.error);
             }
             return req.json();
-        })
-    }
+        });
+}
--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import Notiflix from 'notiflix';
 import { fetchBreeds, fetchCatByBreed } from "./cat-api.js";
 import SlimSelect from 'slim-select';
 
+const selectEl = document.querySelector(".breed-select");
+const loaderEl = document.querySelector(".loader");
+const catInfoDiv = document.querySelector(".cat-info");
+
+selectEl.classList.add("visually-hidden");
+loaderEl.classList.remove("visually-hidden");
 
 fetchBreeds().then(createBreedsList);
 
-const selectEl = document.querySelector(".breed-select");
-selectEl.addEventListener("change", selectElHandler);
-const loaderEl = document.querySelector(".loader");
-
-const catInfoDiv = document.querySelector(".cat-info");
 
 function createBreedsList(data) {
     const result = data.map(({ id, name }) => {
@@ -33,35 +34,45 @@ function createBreedsList(data) {
 }
 
 function selectElHandler(event) {
-    
     const breedId = selectEl.value;
-    
+
     if (breedId === " ") {
         setTimeout(hideLoader, 2500);
         return breedId;
     }
-    fetchCatByBreed(breedId).then(data => {
-        const catImgURL = data[0].url;
-        const catBreedInfo = data[0].breeds[0];
-        const catInfoCode = `
-    <div class="cat-info-box">
-    <img id="photo" class="breed-img" src="${catImgURL}" width="350" loading="lazy" >
-    <div class="cat-text-box"> 
-    <h1 class="breed-name">${catBreedInfo.name}</h1>
-    <p class="breed-description">${catBreedInfo.description}</p>
-    <h2>Temperament:</h2>
-    <p class="breed-temperament"> ${catBreedInfo.temperament}</p></div>
-    </div>
-    `;
-    catInfoDiv.innerHTML = catInfoCode;
-    }).catch(error => {
-        console.log(error);
-    const e = error;
-        Notiflix.Notify.failure(`Error: ${e}`);
-    });
-    setTimeout(hideLoader, 2500);
+
+    loaderEl.classList.remove("visually-hidden");
+
+    fetchCatByBreed(breedId)
+        .then(data => {
+            const catImgURL = data[0].url;
+            const catBreedInfo = data[0].breeds[0];
+            const catInfoCode = `
+                <div class="cat-info-box">
+                    <img id="photo" class="breed-img" src="${catImgURL}" width="350" loading="lazy">
+                    <div class="cat-text-box"> 
+                        <h1 class="breed-name">${catBreedInfo.name}</h1>
+                        <p class="breed-description">${catBreedInfo.description}</p>
+                        <h2>Temperament:</h2>
+                        <p class="breed-temperament">${catBreedInfo.temperament}</p>
+                    </div>
+                </div>
+            `;
+            catInfoDiv.innerHTML = catInfoCode;
+
+            hideLoader();
+        })
+        .catch(error => {
+            console.log(error);
+            const e = error;
+            Notiflix.Notify.failure(`Error: ${e}`);
+
+            hideLoader();
+        });
 }
 
-function hideLoader(){
+function hideLoader() {
     loaderEl.classList.add("visually-hidden");
 }
+
+selectEl.addEventListener("change", selectElHandler);
